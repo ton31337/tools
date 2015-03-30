c40802b7037f9b37f8fa1a5b22d7d009184f3b00
Donatas Abraitis <donatas.abraitis@gmail.com>
2015-03-30 15:51:38 +0300

    Show request size average

--- twemstats.rb
+++ twemstats.rb
@@ -5,6 +5,7 @@ require 'json'
 
 stats = {}
 s_stats = {}
+req_stats = {}
 total = 0
 
 exit! if ARGV[0].nil?
@@ -23,6 +24,7 @@ stats.map { |x|
           if s_val.instance_of? Hash
             num_of_times_server_was_ejected = ((s_val['server_err'] + s_val['server_timedout'] + s_val['server_eof']) / ARGV[0].to_i)
             s_stats.merge!(s_key => num_of_times_server_was_ejected)
+            req_stats.merge!(s_key => (s_val['request_bytes'] / s_val['requests']))
           end
         }
       end
@@ -33,7 +35,7 @@ stats.map { |x|
 # Print stats
 max_size = s_stats.max_by { |k,v| v }.last
 s_stats.map { |srv,count|
-  print "#{srv} ejected--> #{count} times (#{(100 * count) / max_size}%)\n"
+  print "#{srv.split('.').first} E=#{count} (#{(100 * count) / max_size}%), ReqS=#{req_stats[srv]}\n"
   total += count
 }
 print "Total ejection count: #{total}\n"
