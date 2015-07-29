511e66aa35d36ac72bc8dff2e98c0df12598ad53
Donatas Abraitis <donatas.abraitis@gmail.com>
2015-07-29 14:51:12 +0300

    Add stop function

--- backlog_sysdig_all.lua
+++ backlog_sysdig_all.lua
@@ -2,10 +2,23 @@ description = "queue (backlog) utilization"
 short_description = "queue utilization"
 category = "misc"
 
-args = {}
+args =
+{
+  {
+    name = "stop",
+    description = "stop tracing after N seconds",
+    optional = false
+  }
+}
+
+function on_set_arg(name, val)
+    stop_after = tonumber(val)
+    return true
+end
 
 function on_init()
     util = {}
+    start_time = os.time()
     sport = chisel.request_field("fd.sport")
     queue = chisel.request_field("evt.arg[2]")
     sysdig.set_filter("evt.type=accept")
@@ -14,6 +27,7 @@ function on_init()
 end
 
 function on_interval(ts_s, ts_ns, delta)
+  if (os.time() - start_time) > stop_after then sysdig.end_capture() end
   local sum = {}
   local num = {}
   for port,v in pairs(util) do
