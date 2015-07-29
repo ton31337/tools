ffd1a71e5b992bf53704a4ca31ba531d8094dbbc
Donatas Abraitis <donatas.abraitis@gmail.com>
2015-07-29 23:18:46 +0300

    Fix on_interval() with filter and clear terminal

--- backlog_sysdig_all.lua
+++ backlog_sysdig_all.lua
@@ -2,18 +2,31 @@ description = "queue (backlog) utilization"
 short_description = "queue utilization"
 category = "misc"
 
+terminal = require "ansiterminal"
+
 args =
 {
   {
+    name = "refresh",
+    description = "refresh interval",
+    optional = true
+  },
+  {
     name = "stop",
     description = "stop tracing after N seconds",
-    optional = false
-  }
+    optional = true
+  },
 }
 
 function on_set_arg(name, val)
-    stop_after = tonumber(val)
-    return true
+    if name == "stop" then
+      stop_after = tonumber(val)
+      return true
+    elseif name == "refresh" then
+      refresh = tonumber(val)
+      return true
+    end
+    return false
 end
 
 function on_init()
@@ -21,15 +34,24 @@ function on_init()
     start_time = os.time()
     sport = chisel.request_field("fd.sport")
     queue = chisel.request_field("evt.arg[2]")
-    sysdig.set_filter("evt.type=accept")
-    chisel.set_interval_s(1)
+    syscall = chisel.request_field("evt.type")
     return true
 end
 
+function on_capture_start()
+  islive = sysdig.is_live()
+  local refresh = refresh or 1
+  if islive then
+    chisel.set_interval_s(refresh)
+  end
+  return true
+end
+
 function on_interval(ts_s, ts_ns, delta)
-  if (os.time() - start_time) > stop_after then sysdig.end_capture() end
+  local stop_after = stop_after or 10
   local sum = {}
   local num = {}
+  if (os.time() - start_time) > stop_after then sysdig.end_capture() end
   for port,v in pairs(util) do
     local list = v
     sum[port] = 0
@@ -40,6 +62,7 @@ function on_interval(ts_s, ts_ns, delta)
       list = list.next
     end
     if num[port] ~= 0 then
+      terminal.clearscreen()
       print("Utilization for port "..port.." is "..(sum[port] / num[port]).."%")
     end
   end
@@ -49,7 +72,8 @@ end
 
 function on_event()
   idx = evt.field(sport)
-  if idx ~= nil then
+  local syscall = evt.field(syscall)
+  if idx ~= nil and syscall == "accept" then
     util[idx] = { next = util[idx], queue = evt.field(queue) }
   end
   return true
