a8e7d8ffe63966467d6a198e370e1ca5f1b7b9b4
Donatas Abraitis <donatas@opensourcerouting.org>
2023-09-18 15:29:03 +0300

    Handle decompression errors for zlib
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- lua-compress-decompress-performance/decompress-zlib.lua
+++ lua-compress-decompress-performance/decompress-zlib.lua
@@ -1,11 +1,21 @@
 local lz = require "zlib"
 
+function decompress(input)
+  return lz.inflate()(input, "finish")
+end
+
 local file_i = io.open("compressed-zlib.txt", "r")
 local file_o = io.open("decompressed-zlib.txt", "w")
 io.input(file_i)
 io.output(file_o)
 local input = file_i:read("*all")
-local output = lz.inflate()(input, "finish")
+local ok, output = pcall(decompress, input)
+if not ok then
+  print(intput)
+  return
+else
+  print(output)
+end
 io.write(output)
 io.close(file_i)
 io.close(file_o)
