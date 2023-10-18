5419dacf1646ac8770fda04fe02d8fb0f834c791
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-18 11:00:10 +0300

    Adopt spawn-php-fpm.sh to custom chroot example
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
+++ nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-mount --bind /opt/h5g/skeleton/etc/passwd /tmp/passwd
-mount --bind /opt/h5g/skeleton/etc/group /tmp/group
-sudo -u www-data /usr/sbin/php-fpm7.4 --fpm-config /etc/php/h5g/u2.conf
+set -x
+
+CHROOT="/var/lib/machines/debian"
+
+mount --bind /dev/pts/ "${CHROOT}/dev/pts"
+mount --bind /opt/h5g/skeleton/etc/passwd "${CHROOT}/etc/passwd"
+mount --bind /opt/h5g/skeleton/etc/group "${CHROOT}/etc/group"
+mount --bind /opt/h5g/skeleton/etc/php-fpm.conf "${CHROOT}/etc/php-fpm.conf"
+mount --bind /var/www/u2 "${CHROOT}/var/www/u2"
+mount --bind /tmp "${CHROOT}/tmp"
+chroot "${CHROOT}" sudo -u www-data /usr/sbin/php-fpm8.2 --fpm-config /etc/php-fpm.conf
