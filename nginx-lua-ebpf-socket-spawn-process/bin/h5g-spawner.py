a7e961fec8043c2af5f5ed7c6a050d8b6fc931dc
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-20 16:06:41 +0300

    Periodically run reaper function to stop idle php-fpm processes
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- nginx-lua-ebpf-socket-spawn-process/bin/h5g-spawner.py
+++ nginx-lua-ebpf-socket-spawn-process/bin/h5g-spawner.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from bcc import BPF
-import ctypes as ct
 import subprocess
+import time
+import threading
+from datetime import datetime
 
 bcc_prog = """
 #define KBUILD_MODNAME "h5g process spawner"
@@ -42,20 +44,51 @@ void schedule_spawn(struct pt_regs *ctx, int fd, struct sockaddr *uservaddr,
 b = BPF(text=bcc_prog)
 b.attach_kprobe(event="__sys_connect", fn_name="schedule_spawn")
 
+last_seen = {}
+stop_event = threading.Event()
+
 
 def spawn_process(cpu, data, size):
     output = b["events"].event(data)
-    process = subprocess.Popen(
+    sun_path = str(output.sun_path)
+    last_seen[sun_path] = datetime.now().timestamp()
+
+    if ".socket" not in sun_path:
+        return
+
+    print(f"Spawning f{sun_path}")
+    subprocess.Popen(
         ["/opt/h5g/bin/spawn.sh", output.sun_path],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
 
 
+def reap_process_timer():
+    print("Checking idle php-fpm processes...")
+
+
+def reap_process(interval, func, stop_event):
+    def expired():
+        while not stop_event.is_set():
+            func()
+            for _ in range(interval):
+                if stop_event.is_set():
+                    break
+                time.sleep(1)
+
+    thread = threading.Thread(target=expired)
+    thread.start()
+
+
 b["events"].open_perf_buffer(spawn_process)
+
+reap_process(5, reap_process_timer, stop_event)
 
 while True:
     try:
         b.perf_buffer_poll()
     except KeyboardInterrupt:
+        print("Stopping...")
+        stop_event.set()
         exit()
