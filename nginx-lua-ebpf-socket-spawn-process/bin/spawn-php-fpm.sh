250eb1a5485de54261bc953740fe67e66753e289
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-24 17:05:07 +0300

    Use full paths for mount(), chroot()
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
+++ nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
@@ -2,13 +2,15 @@
 # $ shc -f spawn-php-fpm.sh -o spawn-php-fpm
 
 set -x
+set -e
 
 CHROOT="/var/lib/machines/debian"
 
-mount --bind --make-unbindable /dev/pts/ "${CHROOT}/dev/pts"
-mount --bind --make-unbindable /opt/h5g/skeleton/etc/passwd "${CHROOT}/etc/passwd"
-mount --bind --make-unbindable /opt/h5g/skeleton/etc/group "${CHROOT}/etc/group"
-mount --bind --make-unbindable /opt/h5g/skeleton/etc/php-fpm.conf "${CHROOT}/etc/php-fpm.conf"
-mount --bind --make-unbindable /var/www/u2 "${CHROOT}/var/www/u2"
-mount --bind --make-unbindable /tmp "${CHROOT}/tmp"
-chroot "${CHROOT}" sudo -u www-data /usr/sbin/php-fpm8.2 --fpm-config /etc/php-fpm.conf
+/bin/mount --bind --make-unbindable /dev/pts/ "${CHROOT}/dev/pts"
+/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/passwd "${CHROOT}/etc/passwd"
+/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/group "${CHROOT}/etc/group"
+/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/php-fpm.conf "${CHROOT}/etc/php-fpm.conf"
+/bin/mount --bind --make-unbindable /var/www/u2 "${CHROOT}/var/www/u2"
+/bin/mount --bind --make-unbindable /tmp "${CHROOT}/tmp"
+/bin/mount -o remount,noexec,nosuid,nodev "${CHROOT}/tmp"
+/usr/sbin/chroot "${CHROOT}" sudo -u www-data /usr/sbin/php-fpm8.2 --fpm-config /etc/php-fpm.conf
