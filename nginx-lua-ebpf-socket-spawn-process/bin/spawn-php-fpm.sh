ed549351ddcbcd65c23573423cf9efe7cd6aa260
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-25 10:22:54 +0300

    proc mount point must have hidepid=2 option
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
+++ nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # $ shc -f spawn-php-fpm.sh -o spawn-php-fpm
+#
+# Make sure the host has this /proc mount (hidepid=2)
+# mount -o remount,hidepid=2 /proc
 
 set -x
 set -e
@@ -12,5 +15,6 @@ CHROOT="/var/lib/machines/debian"
 /bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/php-fpm.conf "${CHROOT}/etc/php-fpm.conf"
 /bin/mount --bind --make-unbindable /var/www/u2 "${CHROOT}/var/www/u2"
 /bin/mount --bind --make-unbindable /tmp "${CHROOT}/tmp"
+/bin/mount -t proc none "${CHROOT}/proc"
 /bin/mount -o remount,noexec,nosuid,nodev "${CHROOT}/tmp"
 /usr/sbin/chroot "${CHROOT}" sudo -u www-data /usr/sbin/php-fpm8.2 --fpm-config /etc/php-fpm.conf
