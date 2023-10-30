c76f7d8e087e378b0db26ae4ab0ac758b3eac022
Donatas Abraitis <donatas@opensourcerouting.org>
2023-10-30 16:48:53 +0200

    Add network namespace support
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
+++ nginx-lua-ebpf-socket-spawn-process/bin/spawn-php-fpm.sh
@@ -20,19 +20,18 @@
 # Do not forget to do `cgconfigparser -l /etc/cgroup.conf`
 
 set -x
-set -e
 
 CHROOT="/var/lib/machines/debian"
 
-/bin/mount --bind --make-unbindable /dev/pts/ "${CHROOT}/dev/pts"
-/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/passwd "${CHROOT}/etc/passwd"
-/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/group "${CHROOT}/etc/group"
-/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/php-fpm.conf "${CHROOT}/etc/php-fpm.conf"
-/bin/mount --bind --make-unbindable /var/www/u2 "${CHROOT}/var/www/u2"
-/bin/mount --bind --make-unbindable /tmp "${CHROOT}/tmp"
-/bin/mount --type proc none "${CHROOT}/proc" || \
-    /bin/mount -o remount --type proc none "${CHROOT}/proc"
-/bin/mount -o remount,noexec,nosuid,nodev "${CHROOT}/tmp"
+#/bin/mount --bind --make-unbindable /dev/pts/ "${CHROOT}/dev/pts"
+#/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/passwd "${CHROOT}/etc/passwd"
+#/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/group "${CHROOT}/etc/group"
+#/bin/mount --bind --make-unbindable /opt/h5g/skeleton/etc/php-fpm.conf "${CHROOT}/etc/php-fpm.conf"
+#/bin/mount --bind --make-unbindable /var/www/u2 "${CHROOT}/var/www/u2"
+#/bin/mount --bind --make-unbindable /tmp "${CHROOT}/tmp"
+#/bin/mount --type proc none "${CHROOT}/proc" || \
+#    /bin/mount -o remount --type proc none "${CHROOT}/proc"
+#/bin/mount -o remount,noexec,nosuid,nodev "${CHROOT}/tmp"
 /usr/bin/cgexec -g cpu:www-data \
 	/usr/bin/prlimit --nofile=256 --nproc=512 --locks=32 \
 	/sbin/capsh --drop=all --user=www-data --chroot="${CHROOT}" -- -c '/usr/sbin/php-fpm8.2 --fpm-config /etc/php-fpm.conf'
