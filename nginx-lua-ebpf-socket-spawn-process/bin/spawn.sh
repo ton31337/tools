b1b75a4eaa7f1c31680ae84c6d8ac5f40cc345ae
Donatas Abraitis <donatas@opensourcerouting.org>
2023-11-14 15:43:25 +0200

    Remove whitespaces from spawn.sh
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- nginx-lua-ebpf-socket-spawn-process/bin/spawn.sh
+++ nginx-lua-ebpf-socket-spawn-process/bin/spawn.sh
@@ -9,7 +9,7 @@ US_PATH="${1}"
 US_NAME=$(basename "${US_PATH}")
 US_USER="${US_NAME::-7}"
 
-if [[ ${US_PATH} == *".socket"*   ]]; then
+if [[ ${US_PATH} == *".socket"* ]]; then
 	if ! grep "${US_PATH}" /proc/net/unix; then
 		/usr/bin/unshare \
 			--mount \
