d54b2d6c03d917184fe747580385a9a91cccc9c2
Donatas Abraitis <donatas@opensourcerouting.org>
2024-09-04 16:14:35 +0300

    Compile hproxy.c, not flushdb.c
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- redis-module/compile.sh
+++ redis-module/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -fPIC -std=gnu99 -c -o module.o flushdb.c
+gcc -fPIC -std=gnu99 -c -o module.o hproxy.c
 ld -o module.so module.o -shared -Bsymbolic -lc
 cp ./module.so /tmp/module.so
